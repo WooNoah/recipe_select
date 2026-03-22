adf03d696b20de48f92aa818bad8b394609e43dc
De Woo <wwdu2010@gmail.com>
2026-03-22 16:28:14 +0800

    fix: set publicPath for GitHub Pages subpath

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import UniPlugin from '@dcloudio/vite-plugin-uni'
 const uni = (UniPlugin as any).default || UniPlugin
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
+  base: isProduction ? '/recipe_select/' : '/',
   plugins: [uni()],
   build: {
     sourcemap: false
